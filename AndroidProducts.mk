044d3154af227e24f5e6ba22893ac8ae8017feed
JoshuaDoes <joshuadoespersonalstuff@gmail.com>
2019-06-18 02:16:44 -0400

    Use Renouveau makefile

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -3,4 +3,4 @@ PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/lineage_nobleltetmo.mk \
     $(LOCAL_DIR)/aosp_nobleltetmo.mk \
     $(LOCAL_DIR)/viper_nobleltetmo.mk \
-    $(LOCAL_DIR)/rr_nobleltetmo.mk
+    $(LOCAL_DIR)/renouveau_nobleltetmo.mk
