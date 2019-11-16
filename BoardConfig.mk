a635ea545709070a551bc0f0684eba875a213843
ripee <ripee.dev@gmail.com>
2019-11-16 01:30:40 +0100

    nobleltetmo: Set BTM_DEF_LOCAL_NAME

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -29,6 +29,9 @@
 # Assert
 TARGET_OTA_ASSERT_DEVICE := nobleltecan,nobleltespr,nobleltetmo,noblelteusc
 
+# Bluetooth
+BOARD_BLUETOOTH_OID_BUILDCFG_INCLUDE_DIR := device/samsung/nobleltetmo/bluetooth
+
 # Include path
 TARGET_SPECIFIC_HEADER_PATH += device/samsung/nobleltetmo/include
 
