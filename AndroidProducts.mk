baf1b09e0b2deb4d2f726b5701e5a2a9de8712cd
ripee <ripee.dev@gmail.com>
2019-12-21 19:56:11 +0100

    nobleltetmo: Clean up AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,6 +1,2 @@
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/full_nobleltetmo.mk \
-    $(LOCAL_DIR)/lineage_nobleltetmo.mk \
-    $(LOCAL_DIR)/aosp_nobleltetmo.mk \
-    $(LOCAL_DIR)/viper_nobleltetmo.mk \
-    $(LOCAL_DIR)/renouveau_nobleltetmo.mk
+    $(LOCAL_DIR)/lineage_nobleltetmo.mk
