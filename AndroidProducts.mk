03a73f0e06c7f23102874832f902d57e01673218
ripee <ripee.dev@gmail.com>
2020-03-08 21:28:22 +0100

    nobleltetmo: Switch from add_lunch_combo to COMMON_LUNCH_CHOICES

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,2 +1,7 @@
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/lineage_nobleltetmo.mk
+
+COMMON_LUNCH_CHOICES := \
+    lineage_nobleltetmo-user \
+    lineage_nobleltetmo-userdebug \
+    lineage_nobleltetmo-eng
